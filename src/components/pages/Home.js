33c3cec3fff1f848c23b9fc6318f69182d6f4109
ahmetterturk <ahm.ert@hotmail.com>
2021-11-14 21:46:16 +1100

    import section from other folder

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,14 @@ import '../../App.css';
 import Cards from '../Cards/Cards';
 import Footer from '../Footer/Footer';
 import HeroSection from '../HeroSection/HeroSection';
+import Services from '../Services';
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <Cards />
+      <Services />
       <Footer />
     </>
   );
